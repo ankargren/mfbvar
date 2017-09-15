6e293069fe7eddf7b724c7ef228b3e38dbc7d1a1
凱(Kai) <7555828+yukai-yang@users.noreply.github.com>
2017-09-16 00:31:57 +0200

    Update interface.R

--- R/interface.R
+++ R/interface.R
@@ -6,23 +6,27 @@
 #' The functions in the package take the object as the input.
 #' The members of the object can be initialized by the function, and can also be changed by the following "set..." functions.
 #'
-#' 
+#'
 new_MFBVAR <- function(...)
 {
   ret = list()
   class(ret) = "MFBVAR"
-  
+
   return(ret)
 }
 
+#' Set attributes or members in an object of the class MFBVAR
 #'
-#'
+#' @param obj an object of the class MFBVAR
+#' @param ... the attributes and their values to be changed
 #'
 set_Member <- function(obj, ...)
 {
   if(class(obj)!="MFBVAR") stop("message")
-  
-  #
-  
+
+  tmp = list(...)
+  nam = names(tmp)
+  for(iter in 1:length(tmp)) eval(parse(text=paste0("obj$",nam[iter]," = tmp[[iter]]")))
+
   return(obj)
 }
