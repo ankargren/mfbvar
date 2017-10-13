b11d313302caff4637053d6982c9d3be9f942c4a
Sebastian Ankargren <seban876@user.uu.se>
2017-10-13 15:21:19 +0200

    Forgot some include guard.

--- inst/include/mfbvarConfig.h
+++ inst/include/mfbvarConfig.h
@@ -1,5 +1,8 @@
+#ifndef mfbvar__mfbvarConfig__h
+#define mfbvar__mfbvarConfig__h
 #if defined(WIN32) || defined(_WIN32)
    #define ARMA_USE_OPENMP
 #else
    #include <mfbvarConfigGenerated.h>
+#endif
 #endif
