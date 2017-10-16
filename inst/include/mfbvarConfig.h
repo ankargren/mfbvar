4e2ff3325fa96c03c645d0dc577eb839893d439b
Sebastian Ankargren <seban876@user.uu.se>
2017-10-16 12:55:01 +0200

    Trying new way of srtting the variable up from configure. Now also including CFLAGS.

--- inst/include/mfbvarConfig.h
+++
@@ -1,8 +0,0 @@
-#ifndef mfbvar__mfbvarConfig__h
-#define mfbvar__mfbvarConfig__h
-#if defined(WIN32) || defined(_WIN32)
-   #define ARMA_USE_OPENMP
-#else
-   #include "mfbvarConfigGenerated.h"
-#endif
-#endif
