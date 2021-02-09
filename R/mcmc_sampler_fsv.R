efb0b0a24e1345dfb3c221258d8d33885c1a45c5
ankargren <sebastian.ankargren@statistik.uu.se>
2021-02-09 22:02:46 +0100

    Synchronizing initalizations of samplers

--- R/mcmc_sampler_fsv.R
+++ R/mcmc_sampler_fsv.R
@@ -751,34 +751,17 @@ mcmc_sampler.mfbvar_ssng_fsv <- function(x, ...){
   n_vars <- ncol(x$Y)
 
   prior_Pi_Omega <- create_prior_Pi_Omega(x$lambda1, x$lambda2, x$lambda3, x$prior_Pi_AR1, x$Y, x$n_lags)
+  list_to_variables(prior_Pi_Omega, parent.frame(), "prior_Pi_Omega", "prior_Pi_AR1")
   prior_Pi_Omega <- prior_Pi_Omega[-1, ]
-  prior_Pi_AR1 <- x$prior_Pi_AR1
-  prior_zero_mean <- all(x$prior_Pi_AR1 == 0)
+  prior_zero_mean <- all(prior_Pi_AR1 == 0)
 
-  Y <- x$Y
-  freq <- x$freq
-  verbose <- x$verbose
+  list_to_variables(x, parent.frame(), "Y", "freq", "verbose", "prior_psi_mean",
+                    "d", "d_fcst", "check_roots", "n_lags", "n_fcst", "n_reps",
+                    "n_burnin", "n_thin", "n_fac", "freqs", "Lambda_",
+                    "priormu", "priorphiidi", "priorphifac", "priorsigmaidi",
+                    "priorsigmafac", "priorfacload", "restrict")
 
-  prior_psi_mean <- x$prior_psi_mean
-  d <- x$d
-  d_fcst <- x$d_fcst
-  check_roots <- x$check_roots
-  n_determ <- dim(d)[2]
 
-  n_vars <- ncol(Y)
-  n_lags <- x$n_lags
-  n_fac <- x$n_fac
-  n_fcst <- x$n_fcst
-
-  ## Priors
-
-  priormu <- x$priormu
-  priorphiidi <- x$priorphiidi
-  priorphifac <- x$priorphifac
-  priorsigmaidi <- x$priorsigmaidi
-  priorsigmafac <- x$priorsigmafac
-  priorfacload <- x$priorfacload
-  restrict <- x$restrict
 
   if (length(priorsigmaidi) == 1) {
     priorsigmaidi <- rep(priorsigmaidi, n_vars)
@@ -812,128 +795,19 @@ mcmc_sampler.mfbvar_ssng_fsv <- function(x, ...){
   c1 <- ifelse(is.null(x$prior_ng), 0.01, x$prior_ng[2])
   s <- ifelse(is.null(x[["s"]]), 1, x$s)
 
-  ## Initials
+  init_vars <- variable_initialization(Y, freq, freqs, n_lags, Lambda_, n_thin)
+  list_to_variables(init_vars, parent.frame(),
+                    "n_vars", "n_q", "T_b", "n_pseudolags",
+                    "n_T", "n_T_", "n_thin")
 
+  ## Initials
+  pars <- c("Z", "Psi", "Pi", "omega", "phi_mu", "lambda_mu", "mu", "sigma",
+            "phi", "facload", "f", "latent", "latent0")
   add_args <- list(...)
-  n_reps <- x$n_reps
-  n_burnin <- x$n_burnin
-  n_thin <- ifelse(is.null(x$n_thin), 1, x$n_thin)
-  n_thin <- ifelse(!is.null(add_args$n_thin), add_args$n_thin, ifelse(!is.null(x$n_thin), x$n_thin, 1))
-
-  # n_vars: number of variables
-  # n_lags: number of lags
-  # n_determ: number of deterministic variables
-  # n_T: sample size (full sample)
-  # n_T_: sample size (reduced sample)
-
-  freqs <- x$freqs
-  Lambda_ <- x$Lambda_
-
-  n_q <- sum(freq == freqs[1])
-  if (n_q < n_vars) {
-    T_b <- max(which(!apply(apply(Y[, freq == freqs[2], drop = FALSE], 2, is.na), 1, any)))
-  } else {
-    T_b <- nrow(Y)
-  }
-  if (n_q == 0 || n_q == n_vars) {
-    complete_quarters <- apply(Y, 1, function(x) !any(is.na(x)))
-    Y <- Y[complete_quarters, ]
-    d_fcst <- rbind(d[!complete_quarters, , drop = FALSE], d_fcst)
-    d <- d[complete_quarters, , drop = FALSE]
-  }
-
-  n_pseudolags <- max(c(n_lags, ncol(Lambda_)/nrow(Lambda_)))
-  n_T <- dim(Y)[1]# - n_lags
-  n_T_ <- n_T - n_pseudolags
-
-  ## Initials
   init <- add_args$init
-  y_in_p <- Y[-(1:n_lags), ]
-  error_variance <- compute_error_variances(Y)
-
-  ### Regression parameters
-  if (is.null(init$init_Pi)) {
-    init_Pi <- matrix(0, nrow = n_vars, ncol = n_vars*(n_vars*n_lags))
-  } else {
-    init_Pi <- init$init_Pi
-  }
-
-  ### Latent high-frequency
-  if (is.null(init$init_Z)) {
-    init_Z <- fill_na(Y)
-  } else {
-    init_Z <- init$init_Z
-  }
-
-  ### Steady-states
-  if (is.null(init$init_psi)) {
-    init_psi <- prior_psi_mean
-  } else {
-    init_psi <- init$init_psi
-  }
-
-  if (is.null(init$init_omega)) {
-    if (!is.null(x$prior_psi_Omega)) {
-      init_omega <- diag(x$prior_psi_Omega)
-    } else {
-      init_omega <- rep(0.1, n_determ*n_vars)
-    }
-  } else {
-    init_omega <- init$init_omega
-  }
-
-  if (is.null(init$init_phi_mu)) {
-    init_phi_mu <- 1
-  } else {
-    init_phi_mu <- init$init_phi_mu
-  }
-
-  if (is.null(init$init_lambda_mu)) {
-    init_lambda_mu <- 1
-  } else {
-    init_lambda_mu <- init$init_lambda_mu
-  }
-
-  ### SV regressions
-  if (is.null(init$init_mu)) {
-    init_mu <- log(error_variance)
-  } else {
-    init_mu <- init$init_mu
-  }
-  if (is.null(init$init_sigma)) {
-    init_sigma <- rep(0.75, n_vars + n_fac)
-  } else {
-    init_sigma <- init$init_sigma
-  }
-  if (is.null(init$init_phi)) {
-    init_phi <- rep(0.2, n_vars + n_fac)
-  } else {
-    init_phi <- init$init_phi
-  }
-
-  ### Factors and loadings
-  if (is.null(init$init_facload)) {
-    init_facload <-  matrix(rnorm(n_vars*n_fac, sd = 0.5)^2, nrow=n_vars, ncol=n_fac)
-  } else {
-    init_facload <- init$init_facload
-  }
-  if (is.null(init$init_f)) {
-    init_f <-  matrix(rnorm(n_fac*n_T_, sd = 0.5), n_fac, n_T_)
-  } else {
-    init_f <- init$init_f
-  }
-
-  ### Latent volatilities
-  if (is.null(init$init_latent)) {
-    init_latent <-  t(cbind(matrix(c(log(error_variance), rep(1, n_fac)), nrow = n_T_, ncol = n_vars+n_fac, byrow = TRUE)))
-  } else {
-    init_latent <- init$init_latent
-  }
-  if (is.null(init$init_latent0)) {
-    init_latent0 <-  numeric(n_vars + n_fac)
-  } else {
-    init_latent0 <- init$init_latent0
-  }
+  init_pars <- parameter_initialization(Y = Y, n_vars = n_vars, n_lags = n_lags, n_T_ = n_T_,
+                  init = init, n_fac = n_fac, n_determ = n_determ, pars)
+  list_to_variables(init_pars, parent.frame(), paste0("init_", pars))
 
   ################################################################
   ### Preallocation
--- R/utils.R
+++ R/utils.R
@@ -28,3 +28,78 @@ check_required_params <- function(x, ...) {
     stop("Missing elements: ", paste(required_params[which(test)], collapse = " "))
   }
 }
+
+list_to_variables <- function(x, envir, ...) {
+  variables <- unlist(list(...))
+  foo <- mapply(function(x1, x2, envir) assign(x1, x2, envir), names(x[variables]), x[variables], MoreArgs = list(envir = envir))
+  invisible(NULL)
+}
+
+variable_initialization <- function(Y, freq, freqs, n_lags, Lambda_, d = NULL, d_fcst = NULL) {
+  n_vars <- ncol(Y)
+  n_determ <- if (!is.null(d)) dim(d)[2] else NULL
+  n_q <- sum(freq == freqs[1])
+  if (n_q < n_vars) {
+    T_b <- max(which(!apply(apply(Y[, freq == freqs[2], drop = FALSE], 2, is.na), 1, any)))
+  } else {
+    T_b <- nrow(Y)
+  }
+  if (n_q == 0 || n_q == n_vars) {
+    complete_quarters <- apply(Y, 1, function(x) !any(is.na(x)))
+    Y <- Y[complete_quarters, ]
+    if (!is.null(d)) {
+      d_fcst <- rbind(d[!complete_quarters, , drop = FALSE], d_fcst)
+      d <- d[complete_quarters, , drop = FALSE]
+    }
+  }
+
+  n_pseudolags <- max(c(n_lags, ncol(Lambda_)/nrow(Lambda_)))
+  n_T <- dim(Y)[1]# - n_lags
+  n_T_ <- n_T - n_pseudolags
+
+  n_thin <- ifelse(is.null(n_thin), 1, n_thin)
+  return(list(n_vars = n_vars, n_determ = n_determ, n_q = n_q, T_b = T_b,
+              n_pseudolags = n_pseudolags, n_T = n_T, n_T_ = n_T_,
+              n_thin = n_thin))
+}
+
+parameter_initialization <- function(Y, n_vars, n_lags, n_T_, init,
+                                     n_fac = NULL, n_determ = NULL, ...) {
+  arguments <- list(...)
+  parameters <- unlist(arguments)
+  steady_state <- "psi" %in% parameters
+  fsv <- "facload" %in% parameters
+  if (fsv) {
+    error_variance <- compute_error_variances(Y)
+  }
+
+  init_available <- paste0("init", parameters) %in% names(init)
+  init_required <- parameters[!init_available]
+
+  if (any(init_available)) {
+    list_to_variables(init, parent.frame(), paste0("init", parameters)[init_available])
+  }
+
+  for (i in seq_along(init_required)) {
+    initval <- switch(init_required[i],
+                      Z = fill_na(Y),
+                      psi = colMeans(fill_na(Y)),
+                      Pi = matrix(0, nrow = n_vars, ncol = n_vars*(n_vars*n_lags)+!steady_state),
+                      omega = ifelse(!is.null(arguments$prior_psi_Omega),
+                                     diag(prior_psi_Omega),
+                                     rep(0.1, n_determ*n_vars)),
+                      phi_mu = 1,
+                      lambda_mu = 1,
+                      mu = log(error_variance),
+                      sigma = rep(0.75, n_vars + n_fac),
+                      phi = rep(0.2, n_vars + n_fac),
+                      facload = matrix(rnorm(n_vars*n_fac, sd = 0.5)^2, n_vars, n_fac),
+                      f = matrix(rnorm(n_fac * n_T_, sd = 0.5), n_fac, n_T_),
+                      latent = t(cbind(matrix(c(log(error_variance), rep(1, n_fac)), nrow = n_T_, ncol = n_vars+n_fac, byrow = TRUE))),
+                      latent0 <- numeric(n_vars + n_fac)
+    )
+    assign(paste0("init_", init_required[i]), initval, parent.frame())
+  }
+
+  return(mget(paste0("init_", parameters)))
+}
